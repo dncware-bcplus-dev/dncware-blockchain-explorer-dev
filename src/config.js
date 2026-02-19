f40ac819a8f51ca5237527ef3d58b4450f5bd76f
itakura <itakura.hiroyuki@toshiba-sol.co.jp>
2026-02-19 14:34:54 +0900

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025 Toshiba Digital Solutions Corporation.
+// This source code is licensed under the MIT license.
+
+var cfg_chainID = "test.dncware-blockchain.com";
+var cfg_serverURLs = [
+    "https://test1-peer01.test-dncware-blockchain.com/",
+];
+var cfg_BFT = 0;
