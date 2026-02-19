6b4486c157d9493d2edc4ee3bb24e8c42f33df1e
itakura <itakura.hiroyuki@toshiba-sol.co.jp>
2026-02-19 14:48:09 +0900

    デフォルトの接続先を変更

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025 Toshiba Digital Solutions Corporation.
 // This source code is licensed under the MIT license.
 
-var cfg_chainID = "test.dncware-blockchain.com";
+var cfg_chainID = "trial.dncware-blockchain.biz";
 var cfg_serverURLs = [
-    "https://test1-peer01.test-dncware-blockchain.com/",
+    "https://trial1.dncware-blockchain.biz",
 ];
 var cfg_BFT = 0;
